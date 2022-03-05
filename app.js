ed9698a79885993fa450c42fbb2a2596644b539a
William Kwadwo Owusu <williamowusu1998@gmail.com>
2022-03-05 22:07:59 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const app = express()
+
+app.get('/api', (req, res)=>{
+	res.json({
+		"message": 'welcome to the API'
+	});
+});
+
+//route we want to protect with jwt
+app.post("/api/posts", verifyToken,  (req, res)=>{
+	jwt.verify(req.token, 'secretkey', (err, authData)=> {
+		if(err){
+			res.sendStatus(403)
+		} else {
+			res.json({
+				"message": "Post created",
+				authData
+			});
+		}
+	})
+	
+
+});
+
+app.post('/api/login', (req, res) => {
+	//mock user
+	const user = {
+		id: 1,
+		username: "willow",
+		email: "willow@mail.com"
+	}
+	jwt.sign({user}, 'secretkey', {expiresIn: '30s'}, (err, token) => {
+		res.json({
+			token
+		})
+	})
+})
+
+//format of token
+//Authorization: Bearer <access_token>
+
+//verify token
+function verifyToken(req, res, next){
+	//get auth header value
+	const bearerHeader = req.headers['authorization'];
+	//check if bearer is undefined
+	if(typeof bearerHeader !== 'undefined'){
+		//split at space between Bearer and access token
+		const bearer = bearerHeader.split(' ');
+		//get token from array
+		const bearerToken = bearer[1];
+		//set token
+		req.token = bearerToken;
+		//Next middleware
+		next()
+	} else {
+		//forbidden
+		res.sendStatus(403)
+	}
+}
+app.listen(5000, ()=> console.log('Server started on port 5000'))
